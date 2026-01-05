37673d8ab6813ff48b40737949c571696eba1c41
Graham Wakefield <grrrwaaa@gmail.com>
2026-01-05 13:43:58 -0500

    web update

--- index.md
+++ index.md
@@ -6,13 +6,7 @@
 
 **Synopsis:** This course addresses computation as a creative medium from a biologically-inspired standpoint to develop artworks, adaptive media and simulations approaching the fascinating complexity of nature.
 
-**Instructor:** [Graham Wakefield](https://ampd.yorku.ca/profile/graham-wakefield/)
-
-**Zoom ID**: 947 9296 5671 (requires passcode that you received via Teams/email)
-
-[**Class Microsoft Team**](https://teams.microsoft.com/l/team/19%3aef5cfda6bcf0493e8ecae3b0e05d163f%40thread.tacv2/conversations?groupId=2286cd4d-8394-4a66-9c74-0d27ffd875b1&tenantId=34531318-7011-4fd4-87f0-a43816c49bd0)
-
-**Lecture Recordings:** [In the MS Teams folder Files/Class Materials](https://teams.microsoft.com/_#/school/files/General?threadId=19:ef5cfda6bcf0493e8ecae3b0e05d163f@thread.tacv2&ctx=channel)
+**Course details**: See [eClass course](https://eclass.yorku.ca/course/view.php?id=149174)
 
 Artists, composers, designers and architects have always drawn inspiration from nature. In this course we do so by translating *mechanisms inspired by biology* (or theories of biology) into digital/computational media. That is, using bio-inspired computational techniques to leverage or approach the creative capacities of nature. 
 
--- make.js
+++ make.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 
 const fs = require("fs"),
-	path = require("path")
+	path = require("path"),
+	url = require("url"),
+	http = require("http"),
+	assert = require("assert");
+const { networkInterfaces } = require('os');
 
+const express = require("express")
+const WebSocket = require("ws")
 const JSON5 = require("json5")
 const marked = require("marked")
 const hljs   = require('highlight.js')
 const template = require('es6-dynamic-template')
 
-// add some library words to the highlighter:
-hljs.getLanguage("javascript").keywords.built_in += " now dt random wrap shuffle write draw2D field2D vec2 hashspace2D mouse key update draw reset "
+const server_path = __dirname;
+const public_path = server_path; //path.join(server_path, "public");
+const PORT = 8080
 
 const meta_default = {
 	author: "Graham Wakefield",
 	template: "template.html",
 	title: "",
 	description: "",
-	cover_image: "img/collaborative_coding.jpg"
 }
-
 
 function generate(file) {
 	let src = fs.readFileSync(path.format(file), "utf8");
@@ -56,37 +61,45 @@ function generate(file) {
 			.replace(/\n---image:([^\s]+)/g, `\n---\n<!-- .slide: data-background-image="$1" data-background-size="contain" -->\n\nnotes:\n`)
 			// // replace @youtube:ID as background video
 			.replace(/\n---youtube:([^\s]+)/g, `\n---\n<!-- .slide: data-background-interactive data-background-iframe="https://youtube.com/embed/$1?rel=0&autoplay=1&start=0" -->\n\nnotes:\n`)
-
-			
 	} else {
-
 		meta.src = meta.src
 		// auto hr break at heading 1 titles:
-		.replace(/\n(#\s[^\n]+)/g, "\n---\n\n$1")
-		// replace @image:path 
+		.replace(/\n(#\s[^\n]+)/g, '\n---\n<a href="#top"><img src="img/up.png"></a>\n$1')
+		.replace(/\n(##\s[^\n]+)/g, '\n---\n<a href="#top"><img src="img/up.png"></a>\n$1')
+		// replace @image:path as background contain 
 		.replace(/\n---image:([^\s]+)/g, `\n<img src="$1" />\n`)
-		// // replace @youtube:ID 
+		// // replace @youtube:ID as background video
 		.replace(/\n---youtube:([^\s]+)/g, `<iframe width="720" height="540" src="https://youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>`)
 		.replace(/\n---vimeo:([^\s]+)/g, `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/$1?loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`)
+		.replace(/\n---vimeo:([^\s]+)/g, `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/$1?loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`)
 		// auto-embed codepens:
+		.replace(/\nhttps?:\/\/codepen.io\/+([^\/]+)\/pen\/([^\/\n]+)\/?/g, 
+			`<p class="codepen" data-height="520" data-default-tab="js,result" data-user="$1" data-slug-hash="$2" data-preview="true"><span><a href="https://codepen.io/$1/pen/$2">Open pen.</a></span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>`)
 		.replace(/\n---codepen:https?:\/\/codepen.io\/+([^\/]+)\/pen\/([^\/\n]+)\/?/g, 
 			`<p class="codepen" data-height="520" data-default-tab="js,result" data-user="$1" data-slug-hash="$2" data-preview="true"><span><a href="https://codepen.io/$1/pen/$2">Open pen.</a></span></p><script async src="https://static.codepen.io/assets/embed/ei.js"></script>`)
+		// auto-embed youtube e.g. https://www.youtube.com/watch?v=AbcZ2f5fdNc
+		.replace(/\nhttps?:\/\/www.youtube.com\/watch\?v=([^\n\r\/]+)[^\n\r]*/g, `
+<iframe width="720" height="540" src="https://youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>
+`)
+		
+		// auto-embed google slides: e.g. https://docs.google.com/presentation/d/1xrXM86cCE7vzykYYdINs1G9g9f7FaeiiZd6IRlKBEjI/
+		// auto-embed youtube e.g. https://www.youtube.com/watch?v=AbcZ2f5fdNc
+		.replace(/\n\s*(https:\/\/docs.google.com\/presentation\/d\/[^\n]*)/g, 
+			`<p>$1</p>
+			<iframe src="$1embed?start=false" frameborder="0" width="960" height="569" allowfullscreen></iframe>
+`)
 
 	}
-
-	// // https://vimeo.zendesk.com/hc/en-us/articles/360001494447-Using-Player-Parameters
-	// 	.replace(/\n---vimeo:([^\s]+)/g, `\n---\n<!-- .slide: data-background-interactive data-background-iframe="https://player.vimeo.com/video/$1?rel=0&autoplay=1&start=0&muted=0" -->\n\nnotes:\n`)
-
-
+	
 	let toc = []
 	let renderer = new marked.Renderer();
 	let heading = renderer.heading.bind(renderer);
 	renderer.heading = function(text, level, ...args) {
 		const html = heading(text, level, ...args)
 		const match = /id="(.+)"/gm.exec(html)
-		if (match && match.length > 1) {
+		if (match && match.length > 1 && level < 3) {
 			const id = match[1]
-			//console.log(text, level, id)
+			console.log(text, level, id)
 			toc.push({
 				level: level,
 				text: text,
@@ -99,11 +112,12 @@ function generate(file) {
 		renderer: renderer,
 		highlight: function(code, lang) {
 			return hljs.highlight(hljs.getLanguage(lang) ? lang : 'plaintext', code).value;
-		},
+		}
 	});
 
 	meta.body = marked(meta.src);
-	meta.toc = toc.length > 1 ? marked(toc.map(item => `${"  ".repeat(item.level-1)}- [${item.text}](#${item.id})`).join("\n")) : "";
+	//meta.toc = toc.length > 1 ? marked(toc.map(item => `${"  ".repeat(item.level-1)}- [${item.text}](#${item.id})`).join("\n")) : "";
+	// console.log(meta.toc)
 
 	let html = template(fs.readFileSync(meta.template, "utf8"), meta);
 
@@ -113,7 +127,103 @@ function generate(file) {
 	return writename;
 }
 
-console.log("written:", fs.readdirSync(__dirname, "utf8").map(file=>path.parse(file)).filter(file=>file.ext==".md").map(generate))
+console.log("written:", fs.readdirSync(server_path, "utf8").map(file=>path.parse(file)).filter(file=>file.ext==".md").map(generate))
+
+// watch for file changes:
+fs.watch(server_path, (event, filename)=>{
+	console.log(event, filename)
+	let file = path.parse(filename)
+	if (file.ext == ".md") {
+		console.log("generating", file)
+		generate(file);
+		send_all_clients("reload")
+	}
+})
+
+// start a little server:
+
+const app = express();
+app.use(express.static(public_path))
+app.get('/', function(req, res) {
+	res.sendFile(path.join(public_path, 'index.html'));
+});
+
+//app.get('*', function(req, res) { console.log(req); });
+const server = http.createServer(app);
+// add a websocket service to the http server:
+const wss = new WebSocket.Server({ server });
+
+// send a (string) message to all connected clients:
+function send_all_clients(msg) {
+	wss.clients.forEach(client => {
+		try {
+			client.send(msg);
+		} catch (e) {
+			console.error(e);
+		};
+	});
+}
 
 
+// whenever a client connects to this websocket:
+wss.on('connection', function(ws, req) {
+	console.log("server received a connection");
+	console.log("server has "+wss.clients.size+" connected clients");
+	
+	const location = url.parse(req.url, true);
+	// You might use location.query.access_token to authenticate or share sessions
+	// or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+	
+	ws.on('error', function (e) {
+		if (e.message === "read ECONNRESET") {
+			// ignore this, client will still emit close event
+			console.error("websocket ECONNRESET: ", e.message);
+		} else {
+			console.error("websocket error: ", e.message);
+		}
+	});
 
+	// what to do if client disconnects?
+	ws.on('close', function(connection) {
+		console.log("connection closed");
+        console.log("server has "+wss.clients.size+" connected clients");
+	});
+	
+	// respond to any messages from the client:
+	ws.on('message', function(e, isBinary) {
+		if (isBinary) {
+			// get an arraybuffer from the message:
+			const ab = e.buffer.slice(e.byteOffset,e.byteOffset+e.byteLength);
+			console.log("received arraybuffer", ab);
+			// as float32s:
+			//console.log(new Float32Array(ab));
+		} else {
+			e = e.toString()
+			console.log("message", e)
+		}
+		// echo back:
+		ws.send(e);
+    });
+});
+
+const nets = networkInterfaces();
+const selfIPs = []
+const results = Object.create(null); // Or just '{}', an empty object
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+			selfIPs.push(net.address)
+        }
+    }
+}
+
+server.listen(PORT, "127.0.0.1", function() {
+	console.log(server.address())
+	//console.log(`server listening on http://${server.addresns().address}:${server.address().port}`);
+	console.log(`server listening on http://localhost:${server.address().port}`);
+});
